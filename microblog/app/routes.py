858763f2c7f9fa010991bd2f05d0d27c3560c44a
Trey Decker <treydecker75@gmail.com>
2018-10-06 13:05:09 -0600

    tutorial step 3 - midway - improving form validation

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,8 +1,40 @@
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 def default():
     return "Default Page"
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Trey'}
+    posts = [
+        {
+            'author': { 'username': 'John Doe' },
+            'body': 'This is my first blog post'
+        },
+        {
+            'author': { 'username': 'Janie Doe' },
+            'body': 'This is my second blog post'
+        }
+    ]
+    return render_template(
+        'index.html', 
+        title='home', 
+        user=user, 
+        posts=posts
+    )
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data
+        ))
+        return redirect('/index')
+    return render_template(
+        'login.html',
+        title="Login",
+        form=form
+    )
